ea1cf583c79c1a883bc617bf1cba803acce77420
Chi <iewihc@jwnet.com.tw>
2019-08-13 09:14:08 +0800

    TT

---
+++ Server/Connection/idszContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sourcer;
+using System.Configuration;
+
+namespace Server.Connection
+{
+    public class idszContext : DbContext
+    {
+        public idszContext(DbContextOptions<idszContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Student> Student { get; set; }
+        public virtual DbSet<Order> Order { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Flunt api ord_bah. 
+            modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    Id = 1,
+                    FirstName = "Lee",
+                    LastName = "John"
+                },
+                new Student
+                {
+                    Id = 2,
+                    FirstName = "Lee",
+                    LastName = "YueHan"
+                });
+        }
+    }
+}
---
+++ Sourcer/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sourcer
+{
+    //ord_bah1
+    public class Order
+    {
+        [Key]
+        public string cus_nbr { get; set; }
+
+    }
+}
